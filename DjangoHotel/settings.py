74e68d1e7e39ac857d38399cb0ed462381d7f366
JorgenBrattang <jorgen.kvist@gmail.com>
2023-01-11 11:56:34 +0000

    add connection to remote database and its working

--- DjangoHotel/settings.py
+++ DjangoHotel/settings.py
@@ -11,22 +11,33 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21t5b1r+#ysine*w1@88)b+o55a**y64q&qm-^_99zcr6os(f!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+# Added settings
+
+SITE_ID = 1
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Application definition
 
@@ -39,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My_Apps
+    'crispy_forms',
+    'crispy_bootstrap5',
     'hotel',
     'hotel.booking_functions',
 ]
@@ -58,7 +71,7 @@ ROOT_URLCONF = 'DjangoHotel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,16 +87,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'DjangoHotel.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Room, Booking
 
-# Create your views here.
+
+class RoomList(generic.ListView):
+    model = Room
+    template_name = 'room_list.html'
+
+
+class BookingList(generic.ListView):
+    model = Booking
+    template_name = 'booking_list.html'
