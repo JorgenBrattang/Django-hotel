d5f0d2096f67a0c93318ac4492a71cdd6d4f58d2
JorgenBrattang <jorgen.kvist@gmail.com>
2023-01-11 12:48:53 +0000

    Testing if connection to database is error

--- DjangoHotel/settings.py
+++ DjangoHotel/settings.py
@@ -48,6 +48,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
 
+    # AllAuth
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
     # Cloudinary
     'cloudinary_storage',
     'django.contrib.staticfiles',
@@ -59,6 +65,13 @@ INSTALLED_APPS = [
     'hotel',
     'hotel.booking_functions',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
