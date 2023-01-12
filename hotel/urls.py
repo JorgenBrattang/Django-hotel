fd3aab2c58ad0c7ad571edf9a51e2e773b67247f
JorgenBrattang <jorgen.kvist@gmail.com>
2023-01-12 14:54:04 +0000

    Have to quit, room_list_view was the last thing

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import RoomList, BookingList, BookingView, RoomDetail
+from .views import RoomListView, BookingList, BookingView, RoomDetail, RoomDetailView
 
 app_name = 'hotel'
 
 urlpatterns = [
-    path('room_list/', RoomList.as_view(), name='RoomList'),
+    path('room_list/', RoomListView.as_view(), name='RoomList'),
+
+    # Here for checking the html
     path('room_list/room_detail', RoomDetail.as_view(), name='RoomDetail'),
+
+
     path('booking_list/', BookingList.as_view(), name='BookingList'),
-    path('book/', BookingView.as_view(), name='BookingView')
+    path('book/', BookingView.as_view(), name='BookingView'),
+    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView')
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, View
 from .models import Room, Booking
 from .forms import AvailabiltyForm
 from hotel.booking_functions.availability import check_availability
 
 
-class RoomList(ListView):
+class RoomListView(ListView):
     model = Room
     template_name = 'room_list.html'
 
 
+# Here temporary.. I think
 class RoomDetail(ListView):
     model = Room
     template_name = 'detail.html'
@@ -18,6 +19,36 @@ class RoomDetail(ListView):
 class BookingList(ListView):
     model = Booking
     template_name = 'booking_list.html'
+
+
+class RoomDetailView(View):
+    def get(self, request, *args, **kwargs):
+        room_category = self.kwargs.get('category', None)
+        # room_list = Room.objects.filter(category=category)
+        context = {
+            'room_category': room_category
+        }
+        return render(request, 'room_detail_view.html', context)
+
+    def post(self, request, *args, **kwargs):
+        room_list = Room.objects.filter(category=category)
+        available_rooms = []
+        for room in room_list:
+            if check_availability(room, data['check_in'], data['check_out']):
+                available_rooms.append(room)
+
+        if len(available_rooms) > 0:
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user=self.request.user,
+                room=room,
+                check_in=data['check_in'],
+                check_out=data['check_out'],
+            )
+            booking.save()
+            return HttpResponse(booking)
+        else:
+            return HttpResponse('No more free rooms of this category are left at this time.')
 
 
 class BookingView(FormView):
